b26e3dc3fcc3fcc443387ce5cb52d17268f09bba
Luca-Mariselli <lucamariselli@gmail.com>
2024-05-06 17:39:00 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// :rock: MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite (in allegato zip).
+
+// :rock: MILESTONE 1:
+// Stampare su console, per ogni membro del team: il nome e il ruolo.
+
+// :rock: MILESTONE 2:
+// Stampare in pagina, per ogni membro del team: nome, ruolo e immagine, come semplici stringhe.
+// Svolgere questa milestone grazie a una funzione può semplificarvi il lavoro per i bonus. La funzione riceve un oggetto e restituisce una stringa di HTML
+
+
+// :gift: BONUS 1:
+// Trasformare il testo col percorso della foto in una immagine effettiva (in allegato txt).
+// :gift: BONUS 2:
+// Organizzare i singoli membri in card/schede.
+
+let membro0 = {nome:`Wayne Barnett` , ruolo:`Founder & CEO` , immagine:`wayne-barnett-founder-ceo.jpg`}
+let membro1 = {nome:`Angela Caroll` , ruolo:`Chief Editor` , immagine:`angela-caroll-chief-editor.jpg`}
+let membro2 = {nome:`Walter Gordon` , ruolo:`Office Manager` , immagine:`walter-gordon-office-manager.jpg`}
+let membro3 = {nome:`Angela Lopez` , ruolo:`Social Media Manager` , immagine:`angela-lopez-social-media-manager.jpg`}
+let membro4 = {nome:`Scott Estrada` , ruolo:`Developer` , immagine:`scott-estrada-developer.jpg`}
+let membro5 = {nome:`Barbara Ramos` , ruolo:`Graphic Designer` , immagine:`barbara-ramos-graphic-designer.jpg`}
+
+let membri = [
+    membro1,
+    membro2,
+    membro3,
+    membro4,
+    membro5,
+    membro6
+]
